8072ca5786f775d169b3c54d61b490392fd78d93
ThomasRAYNAUD <toto.raynaud@sfr.fr>
2024-01-25 15:30:28 +0100

    Nbr player & génération carte aléatoirement

--- game.py
+++ game.py
@@ -5,31 +5,49 @@ import signal
 from multiprocessing import Process
 import multiprocessing as mp
 import sys
+import select
+
+# variables gloables : 
 
 
+#
+# Défintions des classes
+#
+
+"""carte = un tuple"""
 Carte = (str, int)
 
+"""pile de cartes d'une couleur spécifique"""
 class Pile:
     def __init__(self, couleur): 
         self.couleur = couleur
         self.valeur = 0
 
-
+"""joueur avec un nom et une liste de cartes"""
 class Joueur:
-    def __init__(Joueur, nom, cartes):  
+    def __init__(self, nom, cartes):  
         Joueur.nom = nom
         Joueur.cartes = cartes
+
+
 class Jetons:
     def __init__(self, nombre_de_joueurs):
         self.infos = 3 + nombre_de_joueurs
         self.fuse = 3
 
-def creer_jeu_de_cartes():
+#
+# Défintions des fonctions
+# 
+
+"""Un jeu de cartes est créé avec la fonction creer_jeu_de_cartes() --> appeler au début du jeu"""
+def creer_jeu_de_cartes(nombre_joueurs):
     couleurs = ["r", "b", "v", "j", "n"]
+    couleurs_joueurs = random.sample(couleurs, k=nombre_joueurs) # prend aléatoirement X couleurs, où X est le nbr de joueurs
+
     nombres = [1, 2, 3, 4, 5]
     cartes = []
 
-    for couleur in couleurs:
+    for couleur in couleurs_joueurs:
         for nombre in nombres:
             if nombre == 1:
                 nb_exemplaires = 3
@@ -40,41 +58,11 @@ def creer_jeu_de_cartes():
             for _ in range(nb_exemplaires):
                 cartes.append((couleur, nombre))
 
+    # Mélanger aléatoirement les cartes
+    random.shuffle(cartes)
+
     return cartes
 
-
-def tirer_mains_des_joueurs(nombre_de_joueurs, jeu_de_cartes):
-    return {
-        f"Joueur {i+1}": Joueur(f"Joueur {i+1}", random.sample(jeu_de_cartes, 5))
-        for i in range(nombre_de_joueurs)
-    }
-
-cartes = creer_jeu_de_cartes()
-print(cartes)
-
-nombre_de_joueurs = 2
-jetons = Jetons(nombre_de_joueurs)
-
-couleurs = ["R", "B", "V", "J", "N"]
-piles_couleurs = {couleur: Pile(couleur) for couleur in couleurs[:nombre_de_joueurs]}
-
-joueurs = tirer_mains_des_joueurs(nombre_de_joueurs, cartes)
-
-for joueur, info in joueurs.items():
-    print(f"{info.nom} :")
-    for carte in info.cartes:
-        print(f"Carte : {carte[0]} - Numéro : {carte[1]}")
-
-print("Piles Couleurs :")
-for pile in piles_couleurs.values():
-    print(f"{pile.couleur}, {pile.valeur}")
-
-
-print("Jetons disponibles :")
-print("Jetons Fuse :", jetons.fuse, "\nJetons Infos :", jetons.infos)
-
-HOST = "localhost"
-PORT = 25425
 
 def handler(sig, frame):
 
@@ -82,57 +70,57 @@ def handler(sig, frame):
         print("Received SIGINT. Exiting...")
         sys.exit()
 
-def connection(client_socket, address, joueurs):
-    with client_socket:
-        print(f"Connected to client: {address}")
+def send(type):
+    # selon le type, un choix différent est fait = message envoyé différent
+    if type == "N":  # demande le nombre de joueurs dans la partie
+        m = type + "|" + "Combien de joueurs dans la partie ? "
+        client_socket.sendall(m.encode('utf-8'))
 
-        for joueur, info in joueurs.items():
-            data = f"Joueur {info.nom} : {info.cartes}\n"
-            client_socket.send(data.encode('utf-8'))
-            print(data)
-        client_socket.send(b"FIN")
+def decode(message):
+    # décode la trame pour en sortir tous les éléments et le mettre dans un tableau
+    message_parts = message.decode('utf-8').split("|")
+    return message_parts
 
-        print(f"Disconnecting from client: {address}")
+def type(tab):
+    # regarde le premier élément du tableau (= type) et en fonction prend une décision
+    if tab[0] == "N":
+        return tab[1]  # retourne le nombre de joueurs
+
+def receive():
+    data = client_socket.recv(1024)
+    return decode(data)
+
+def nombrePlayer():
+    try:
+        send("N")
+        tab_data = receive()
+        return type(tab_data)
+    except Exception as e:
+        print(f"Erreur lors de la communication avec le client : {e}")
+        return None
+
+####### MAIN
 
 if __name__ == "__main__":
+    # appelle la fonction handle si SIGINT est reçu
     signal.signal(signal.SIGINT, handler)
     
-    # Configuration du socket du serveur
+    # Créer Socket
+    HOST = "localhost"
+    PORT = 25425
+    # Configuration du socket du serveur, un seul process car une seule connection à maintenir dans le socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        ADDR = (HOST, PORT)
-        server_socket.bind(ADDR)
-        server_socket.listen(1)
-        print(f"Server is listening on {HOST}:{PORT}")
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # régler problème d'adresse déjà utilisée
+        server_socket.bind((HOST,PORT))
+        server_socket.listen(1) # communique avec un seul process Player (orchestrateur)
+        client_socket, address = server_socket.accept()
+        with client_socket:
+            print("Connected to client: ", address)
+            print("Je demande le nombre de joueurs.")
+            n_player=nombrePlayer()
+            print("Le nombre de joueur est le suivant :",n_player)
+            print(creer_jeu_de_cartes(int(n_player)))
+            
 
-        while True:
-            print("Waiting for a connection...")
-            client_socket, address = server_socket.accept()
-            p = Process(target=connection, args=(client_socket, address, joueurs))
-            p.start()
-
-
-"""""
-METTRE LES JETONS DANS UNE SHARED MEMORY ET AJOUTER UN LOCK
-nombre de joueur = nombre de couleur
-3 jetons infos + 3 jetons fusions
-donner une info  = jetons_informations -1
-carte joué si la couleur existe pas ou le numéro (si carte joué est 5 = +1 jetons_informations)
-si pas de carte joué -> pioche une carte
-sinon jeton_fusions-1
-fin du jeu si : plus de jetons fusion ou tous les piles ont été faites 
-
-construire la pile de 1 à 5. première équipe qui finit sa pile de couleur a gagné
-
-faire 2 processes : 
-
-un process jeu  : il contient la pioche, gestion des cartes, suivi des piles 
-un process player : connecté avec les autres joueurs et le process jeu. Il communique aux autres processes ses cartes et ses jetons.
-Interaction avec le process jeu sur un tread différent que les autres.
-
-definir les types
-
-MESSAGE QUEUE ENTRE LES PROCESS ENFANTS ET SOCKET ENTRE PLAYER PROCESS ET GAME PROCESS
-le socket envoie au player qui lui va redistribuer aux joueurs la trame envoyée
-socketclient.py
-"""
+    # Poser la question au process Player ("nbr de joueur ?")
+    # met dans une varible globale
